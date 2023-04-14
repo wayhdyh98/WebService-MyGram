e189dfae3b67f2496f9d6ae9dfc0d89256cb15f4
wayhdyh98 <mpmdev2020@gmail.com>
2023-04-14 08:57:08 +0700

    update to authorization for comment

--- middlewares/authorization.go
+++ middlewares/authorization.go
@@ -10,7 +10,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Authorization() gin.HandlerFunc {
+func PhotoAuthorization() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		db := database.GetDB()
 		photoId, err := strconv.Atoi(c.Param("photoId"))
@@ -38,6 +38,45 @@ func Authorization() gin.HandlerFunc {
 		}
 
 		if Photo.UserId != userId {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error":   "Unauthorized",
+				"message": "Access is denied!",
+			})
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func CommentAuthorization() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		db := database.GetDB()
+		commentId, err := strconv.Atoi(c.Param("commentId"))
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error":   "Bad Request",
+				"message": "Invalid parameter!",
+			})
+			return
+		}
+
+		userData := c.MustGet("userData").(jwt.MapClaims)
+		userId := uint(userData["id"].(float64))
+		Comment := models.Comment{}
+
+		err = db.Select("user_id").First(&Comment, uint(commentId)).Error
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error":   "Data Not Found",
+				"message": "Data doesn't exist!",
+			})
+			return
+		}
+
+		if Comment.UserId != userId {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
 				"error":   "Unauthorized",
 				"message": "Access is denied!",
