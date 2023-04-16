a941208b9003bfc1fa91a4332f01bcfa54377dd2
Wahyu Hidayah <spongebob.krabby@gmail.com>
2023-04-16 15:41:50 +0700

    modif user model

--- models/user.go
+++ models/user.go
@@ -10,7 +10,7 @@ type User struct {
 	GormModel
 	Username    string        `gorm:"not null;uniqueIndex" json:"username" form:"username" valid:"required~Username is required!"`
 	Email       string        `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"required~Email is required!,email~Invalid format email!"`
-	Password    string        `gorm:"not null" json:"-" form:"password" valid:"required~Password is required!,minstringlength(6)~Password minimum length must be 6 characters!"`
+	Password    string        `gorm:"not null" json:"password" form:"password" valid:"required~Password is required!,minstringlength(6)~Password minimum length must be 6 characters!"`
 	Age         int           `gorm:"not null" json:"age" form:"age" valid:"required~Age is required!,range(9|99)~Age minimum value is 9"`
 	Photo       []Photo     
 	Comment     []Comment     
